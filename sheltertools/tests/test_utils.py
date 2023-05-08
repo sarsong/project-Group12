10c7664531729b7d20a673e09a776c3481763ccc
Michelle Lin <lin.michelle@berkeley.edu>
2023-05-08 22:47:12 +0000

    save plots to figures file

--- sheltertools/tests/test_utils.py
+++ sheltertools/tests/test_utils.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sheltertools.utils import *
+import pytest
+
+
 
 
 shelter_data = pd.read_csv('data/Animal_Shelter_Data.csv')
@@ -15,4 +18,16 @@ def test_number_of_animals_by_type():
 	# Assert that the output is a pandas Series
 	assert type(output) == pd.core.series.Series, "The output is not a pandas Series."
 
+	
 # Test 2
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_plot_title():
+	"""
+	Check whether the plot has the correct title
+	"""
+	expected_title = 'Top DOG Breeds in the Shelter'
+	plot_top_breeds('DOG', shelter_data)
+	actual_title = plt.gca().get_title()
+	assert actual_title == expected_title, f"Unexpected title. Expected: {expected_title}, Actual: {actual_title}"
+	
+#def test_
--- sheltertools/utils.py
+++ sheltertools/utils.py
@@ -16,10 +16,13 @@ def plot_top_breeds(animal_type, shelter_data):
     """
     animal_data = shelter_data[shelter_data['Type'] == animal_type]
     animal_breed_counts = animal_data['Breed'].value_counts(ascending=True).tail(5)
+    plt.figure(figsize=(10,6))
     plt.barh(animal_breed_counts.index, animal_breed_counts)
     plt.title('Top ' + animal_type +  ' Breeds in the Shelter', size=15)
     plt.xlabel('Count', size=15)
     plt.ylabel('Breed', size=15)
+    plt.tight_layout()
+    plt.savefig('figures/'+animal_type+'_in_the_Shelter')
     plt.show()
 	
 def plot_trend_line(shelter_data):
@@ -31,11 +34,14 @@ def plot_trend_line(shelter_data):
     shelter_data = shelter_data[shelter_data['Intake Year'] != '2013']
     intake_counts = shelter_data.groupby('Intake Year').size()
     intake_counts.plot(kind='line', title='Animal Intakes by Year')
+    plt.figure(figsize=(10,6))
     plt.scatter(x=intake_counts.index, y=intake_counts)
     plt.xticks(np.arange(len(intake_counts)), intake_counts.index)
     plt.xlabel('Intake Year', size=15)
     plt.ylabel('Count', size=15)
     plt.title('Trend Line of Animal Intakes by Year', size=15)
+    plt.tight_layout()
+    plt.savefig('figures/'+'Trend_Line_of_Animal_Intakes_by_Year')
     plt.show()
 	
 def trend_proportion(Outcome, yearly_rate):
@@ -46,3 +52,6 @@ def trend_proportion(Outcome, yearly_rate):
     ax = outcome_type.plot(x="year",y="percent")
     ax.set_ylabel("% out of total intakes")
     ax.set_title("Proportion of "+Outcome+" by year")
+    fig = ax.get_figure()
+    fig.set_size_inches(8, 6) 
+    fig.savefig('figures/' + 'Proportion_of_'+ Outcome + '_by_Year', bbox_inches='tight')
